cce05602ff1dc9e97b02deb8910942eb26cd44a5
roman-bialek <roman.bialek@postgrad.manchester.ac.uk>
2025-09-03 14:41:43 +0100

    Remove shebang-text; slurm may conflict?

--- regex_extraction.sh
+++ regex_extraction.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+
 
 # make the headers of the csv
-log_file_name=$1; #log.solver # <---- change this
+log_file=$1; #log.solver # <---- change this
 
 echo "simulation_time" > tmp_simtime
 echo "total_cells" > tmp_total_cells
@@ -9,19 +9,19 @@ echo "total_cells" > tmp_total_cells
 # echo "-1" >> tmp_total_cells # offset with dummy value - manually change this to value of choice
 
 # Using `>>` to append at end of file
-grep -oP '(?<=^Time = )[+\-]?(\d+\.\d*|\d*\.\d+|\d+)([eE][+\-]?\d+)?' $log_file_name >> tmp_simtime
+grep -oP '(?<=^Time = )[+\-]?(\d+\.\d*|\d*\.\d+|\d+)([eE][+\-]?\d+)?' $log_file >> tmp_simtime
 
-# grep -oP '^Refined from \d+ to (\d+) cells\.' $log_file_name > tmp_total_cells
+# grep -oP '^Refined from \d+ to (\d+) cells\.' $log_file > tmp_total_cells
 ## update:
-# grep -oP '^Refined from \d+ to \K\d+(?= cells\.)|^Selected 0 cells for refinement out of \K\d+' $log_file_name >> tmp_total_cells
+# grep -oP '^Refined from \d+ to \K\d+(?= cells\.)|^Selected 0 cells for refinement out of \K\d+' $log_file >> tmp_total_cells
 grep -oP '^Refined from \d+ to \K\d+(?= cells\.)|^Unrefined from \d+ to \K\d+(?= cells\.)|Selected 0 cells for refinement out of \K\d+' \
-         $log_file_name >> tmp_total_cells 
+         $log_file >> tmp_total_cells 
 
 # get exec and clock times
 echo "execution_time" > tmp0
 echo "clock_time" > tmp1
-grep -oP '(?<=ExecutionTime = )[+\-]?(\d+\.\d*|\d*\.\d+|\d+)([eE][+\-]?\d+)?' $log_file_name >> tmp0
-grep -oP '(?<=ClockTime = )[0-9.]+' $log_file_name >> tmp1
+grep -oP '(?<=ExecutionTime = )[+\-]?(\d+\.\d*|\d*\.\d+|\d+)([eE][+\-]?\d+)?' $log_file >> tmp0
+grep -oP '(?<=ClockTime = )[0-9.]+' $log_file >> tmp1
 
 # merge files together by columns
 paste -d "," tmp_simtime tmp_total_cells tmp0 tmp1 > data.csv
