fbb523304d48315f8d89ff5b52fb985372f92653
roman-bialek <roman.bialek@postgrad.manchester.ac.uk>
2025-09-04 21:33:45 +0100

    Update/cleanup script
    Presently last total_cell count in last row will be missing; will be fixed in the future

--- regex_extraction.sh
+++ regex_extraction.sh
@@ -4,21 +4,32 @@
 log_file=$1; #log.solver # <---- change this
 refinement_interval=$2 # 10
 
+callas_example=">  bash ${0} logfile refinement_interval_value"
+if [ -z ${log_file} ]; then
+    echo "${0} Error:"
+    echo ">  log file not provided in 1st position"
+    echo ">  Please see example below:"
+    echo ">  (call as:)"
+    echo ${callas_example}
+    exit 1
+fi
+
+if [ -z ${refinement_interval} ]; then
+    echo "${0} Error:"
+    echo ">  Refinement interval not provided in 2nd position"
+    echo ">  Please provide a value, e.g., 1, 2, ..., 10"
+    echo "> (call as:)"
+    echo ${callas_example}
+    exit 1
+fi
 
 echo "simulation_time" > tmp_simtime
 echo "total_cells" > tmp_total_cells
 
-# echo "-1" >> tmp_total_cells # offset with dummy value - manually change this to value of choice
 
 # (Using `>>` to append at end of file)
 echo "(Grep 1: sim time)"
 grep -oP '(?<=^Time = )[+\-]?(\d+\.\d*|\d*\.\d+|\d+)([eE][+\-]?\d+)?' $log_file >> tmp_simtime
-
-    # grep -oP '^Refined from \d+ to (\d+) cells\.' $log_file > tmp_total_cells
-    ## update:
-    # grep -oP '^Refined from \d+ to \K\d+(?= cells\.)|^Selected 0 cells for refinement out of \K\d+' $log_file >> tmp_total_cells
-#grep -oP '^Refined from \d+ to \K\d+(?= cells\.)|^Unrefined from \d+ to \K\d+(?= cells\.)|Selected 0 cells for refinement out of \K\d+' \
-#         $log_file >> tmp_total_cells
 
 
 echo ""
@@ -38,10 +49,6 @@ python duplicate_columns.py edge_case_out.log ${refinement_interval} # returns f
 echo "total_cell" > tmp_add_header
 cat tmp_add_header tmp_total_cell_fixed > tmp_total_cells
 
-# mv tmp_total_cells tmp_total_cells0
-# rm tmp_total_cells
-# mv tmp_total_cell_fixed_w_header tmp_total_cells
-
 
 # get exec and clock times
 echo "execution_time" > tmp0
@@ -51,11 +58,19 @@ grep -oP '(?<=ExecutionTime = )[+\-]?(\d+\.\d*|\d*\.\d+|\d+)([eE][+\-]?\d+)?' $l
 echo "(grep 3: clock times)"
 grep -oP '(?<=ClockTime = )[0-9.]+' $log_file >> tmp1
 
+
 # merge files together by columns
 paste -d "," tmp_simtime tmp_total_cells tmp0 tmp1 > data.csv
 
-# optional (clean up)
-# rm tmp_simtime tmp_total_cells tmp0 tmp1
+
+# optional (clean up; un/comment)
+rm tmp_simtime \
+    tmp_total_cells \
+    tmp0 \
+    tmp1 \
+    tmp_add_header \
+    tmp_total_cell_fixed \
+    edge_case_out.log
 
 echo ""
 echo "(Exit 0, End of Script)"
