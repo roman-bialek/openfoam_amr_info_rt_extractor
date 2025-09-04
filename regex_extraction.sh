1d191e4f0b6cfd70707fdacb54c46054dcef4a07
roman-bialek <roman.bialek@postgrad.manchester.ac.uk>
2025-09-04 20:42:17 +0100

    Update/Add regex_extraction

--- regex_extraction.sh
+++ regex_extraction.sh
@@ -11,11 +11,22 @@ echo "total_cells" > tmp_total_cells
 # Using `>>` to append at end of file
 grep -oP '(?<=^Time = )[+\-]?(\d+\.\d*|\d*\.\d+|\d+)([eE][+\-]?\d+)?' $log_file >> tmp_simtime
 
-# grep -oP '^Refined from \d+ to (\d+) cells\.' $log_file > tmp_total_cells
-## update:
-# grep -oP '^Refined from \d+ to \K\d+(?= cells\.)|^Selected 0 cells for refinement out of \K\d+' $log_file >> tmp_total_cells
-grep -oP '^Refined from \d+ to \K\d+(?= cells\.)|^Unrefined from \d+ to \K\d+(?= cells\.)|Selected 0 cells for refinement out of \K\d+' \
-         $log_file >> tmp_total_cells 
+    # grep -oP '^Refined from \d+ to (\d+) cells\.' $log_file > tmp_total_cells
+    ## update:
+    # grep -oP '^Refined from \d+ to \K\d+(?= cells\.)|^Selected 0 cells for refinement out of \K\d+' $log_file >> tmp_total_cells
+#grep -oP '^Refined from \d+ to \K\d+(?= cells\.)|^Unrefined from \d+ to \K\d+(?= cells\.)|Selected 0 cells for refinement out of \K\d+' \
+#         $log_file >> tmp_total_cells
+
+python cell_regex.py ${log_file} -o edge_case_out.log --verbose
+
+# Fix tmp_total_cells
+ python duplicate_columns.py edge_case_out.log 10
+#tmp_total_cell_fixed
+
+mv tmp_total_cells tmp_total_cells0
+rm tmp_total_cells
+mv tmp_total_cell_fixed tmp_total_cells
+
 
 # get exec and clock times
 echo "execution_time" > tmp0
@@ -27,4 +38,4 @@ grep -oP '(?<=ClockTime = )[0-9.]+' $log_file >> tmp1
 paste -d "," tmp_simtime tmp_total_cells tmp0 tmp1 > data.csv
 
 # optional (clean up)
-rm tmp_simtime tmp_total_cells tmp0 tmp1
+# rm tmp_simtime tmp_total_cells tmp0 tmp1
